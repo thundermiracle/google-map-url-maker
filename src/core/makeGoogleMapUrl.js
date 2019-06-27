34460070dbd8aed351f2f2e690e19ee2f2668107
thundermiracle <cissoliu@gmail.com>
2019-06-27 10:38:29 +0900

    change map's baseurl from maps/search to maps?q=

--- src/core/makeGoogleMapUrl.js
+++ src/core/makeGoogleMapUrl.js
@@ -1,5 +1,5 @@
 export default function makeGoogleMapUrl(prefecture, city, address) {
-  const baseUrl = 'https://www.google.com/maps/search/?api=1&query=';
+  const baseUrl = 'https://www.google.com/maps?q=';
   const fullAddress = `${prefecture}${city}${address}`;
 
   return `${baseUrl}${encodeURIComponent(fullAddress)}`;
